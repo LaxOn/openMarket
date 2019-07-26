0a2442a7bf956eb53dd563218f6a66e156d32904
LaxOn <br_lacson@yahoo.com>
2019-07-25 23:24:02 -0400

    removed instruction comment

--- open_market.py
+++ open_market.py
@@ -1,19 +1,3 @@
-# It was a pleasure talking to you today. As we discussed, here's the task that you need to code so I can share it with my developers.
- 
-# Use case:
-# A Lenmo borrower would like to borrower $5,000.00 on paying them back on 6 months period. One of Lenmo investors has offered him 15% Annual Interest Rate. A $3.00 Lenmo fee will be added to the total loan amount to be paid by the investor.  
- 
-# Requirements:
-# You are required to develop a code using Python language for the following flow;
- 
-# The borrower submits a loan request including the above loan amount and loan period 
-# The investor will submit an offer with the above interest rate
-# The borrower will accept the offer
-# Check if the investor has sufficient balance in their account before they fund the loan
-# The loan will be funded successfully and the loan status will be changed to `Funded` 
-# The loan payment will be scheduled successfully on the system
-# Once all the payments are successfully paid, the loan status will be `Completed`  
-
 class OpenMarket:
 	borrowers = {}
 	investors = {}
